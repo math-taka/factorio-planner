7b2e8ad47b6c1d0f12bd94eef380a32ae9ca5c0f
math-taka <mass.11235813@gmail.com>
2026-07-11 14:26:00 +0000

    Implement RecipeBook class

--- src/main/java/factorio/Main.java
+++ src/main/java/factorio/Main.java
@@ -8,9 +8,9 @@ import factorio.model.Recipe;
 public class Main{
     public static void main(String[] args){
 
-        ItemStack iron = new ItemStack("Iron Plate", 10);
+        ItemStack needs = new ItemStack("Iron Plate", 10);
 
-        System.out.println(iron);
+        System.out.println(needs);
 
         ItemStack ironOre = new ItemStack("Iron Ore",1);
         ItemStack ironPlate = new ItemStack("Iron Plate",1);
---
+++ src/main/java/factorio/model/RecipeBook.java
@@ -0,0 +1,30 @@
+package factorio.model;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+public class RecipeBook{
+
+    private final Map<String,Recipe> book;
+
+    public RecipeBook(Map<String,Recipe> book){
+        this.book = Map.copyOf(book);
+    }
+
+    public RecipeBook(List<Recipe> recipes){
+        Map<String,Recipe> map=new HashMap<String,Recipe>();
+
+        for(Recipe recipe:recipes){
+            for(ItemStack product:recipe.products()){
+                map.put(product.item(),recipe);
+            }
+        }
+
+        this.book = Map.copyOf(map);
+    }
+
+    public Recipe getRecipe(String item){
+        return book.get(item);
+    }
+}
