d6140551fd2ff9074aaacade019d4aee45543565
math-taka <mass.11235813@gmail.com>
2026-07-18 23:28:58 +0000

    create calculatesIronPlateIngeredients test

--- src/test/java/factorio/calculator/RecipeCalculatorTest.java
+++ src/test/java/factorio/calculator/RecipeCalculatorTest.java
@@ -3,10 +3,30 @@ package factorio.calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import factorio.model.*;
+import java.util.List;
+
 public class RecipeCalculatorTest {
     
     @Test
     void sampleTest(){
         assertEquals(2,1+1);
+    }
+
+    @Test
+    void calculatesIronPlateIngredients(){
+        ItemStack need = new ItemStack("Iron Plate",10);
+        
+        ItemStack ironPlateForRecipe = new ItemStack("Iron Plate",1);
+        ItemStack ironOreForRecipe = new ItemStack("Iron Ore",1);
+
+        Recipe ironPlateRecipe = new Recipe(List.of(ironOreForRecipe),List.of(ironPlateForRecipe),3.2);
+        RecipeBook book =new RecipeBook(List.of(ironPlateRecipe));
+
+        RecipeCalculator calculator= new RecipeCalculator();
+
+        List<ItemStack> expected = List.of(new ItemStack("Iron Ore",10));
+        assertEquals(expected,calculator.calculateIngredients(need, book));
+
     }
 }
