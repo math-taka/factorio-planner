6c3da273ef3a8fb78f57f821d805c12146cef126
math-taka <mass.11235813@gmail.com>
2026-07-18 23:09:30 +0000

    create sample test

---
+++ src/test/java/factorio/calculator/RecipeCalculatorTest.java
@@ -0,0 +1,12 @@
+package factorio.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class RecipeCalculatorTest {
+    
+    @Test
+    void sampleTest(){
+        assertEquals(2,1+1);
+    }
+}
