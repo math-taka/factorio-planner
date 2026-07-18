872cab0576b5eb4dece6a94dfa97d06da024ea26
math-taka <mass.11235813@gmail.com>
2026-07-18 23:54:11 +0000

    setting setUp method

--- src/test/java/factorio/calculator/RecipeCalculatorTest.java
+++ src/test/java/factorio/calculator/RecipeCalculatorTest.java
@@ -1,12 +1,51 @@
 package factorio.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import factorio.model.*;
 import java.util.List;
 
 public class RecipeCalculatorTest {
+
+    private RecipeBook book;
+    private RecipeCalculator calculator;
+
+    @BeforeEach
+    void setUp(){
+        ItemStack ironPlateForRecipe = new ItemStack("Iron Plate",1);
+        ItemStack ironOreForIronPlate = new ItemStack("Iron Ore",1);
+
+        Recipe ironPlateRecipe = new Recipe(
+            List.of(ironOreForIronPlate),
+            List.of(ironPlateForRecipe),
+            3.2
+        );
+
+        ItemStack GearWheelForRecipe = new ItemStack("GearWheel",1);
+        ItemStack ironPlateForGear = new ItemStack("Iron Plate",2);
+
+        Recipe GearWheelRecipe = new Recipe(
+            List.of(ironPlateForGear),
+            List.of(GearWheelForRecipe),
+            0.5
+        );
+
+        ItemStack transportBeltForRecipe = new ItemStack("Transport Belt",2);
+        ItemStack ironPlateForBelt = new ItemStack("Iron Plate",1);
+        ItemStack GearWheelForBelt = new ItemStack("Gear Wheel",1);
+
+        Recipe transportBeltRecipe = new Recipe(
+            List.of(ironPlateForBelt,GearWheelForBelt),
+            List.of(transportBeltForRecipe),
+            0.5
+        );
+
+        book = new RecipeBook(List.of(ironPlateRecipe,GearWheelRecipe,transportBeltRecipe));
+        calculator = new RecipeCalculator();
+    }
     
     @Test
     void sampleTest(){
@@ -16,16 +55,8 @@ public class RecipeCalculatorTest {
     @Test
     void calculatesIronPlateIngredients(){
         ItemStack need = new ItemStack("Iron Plate",10);
-        
-        ItemStack ironPlateForRecipe = new ItemStack("Iron Plate",1);
-        ItemStack ironOreForRecipe = new ItemStack("Iron Ore",1);
-
-        Recipe ironPlateRecipe = new Recipe(List.of(ironOreForRecipe),List.of(ironPlateForRecipe),3.2);
-        RecipeBook book =new RecipeBook(List.of(ironPlateRecipe));
-
-        RecipeCalculator calculator= new RecipeCalculator();
-
         List<ItemStack> expected = List.of(new ItemStack("Iron Ore",10));
+        
         assertEquals(expected,calculator.calculateIngredients(need, book));
 
     }
