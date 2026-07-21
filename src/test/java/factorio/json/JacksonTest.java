e71872390d07bbefd24448fe1fd7256464438ea5
math-taka <mass.11235813@gmail.com>
2026-07-21 07:45:33 +0000

    setting readRecipeTest

--- src/test/java/factorio/json/JacksonTest.java
+++ src/test/java/factorio/json/JacksonTest.java
@@ -2,20 +2,32 @@ package factorio.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.util.List;
+
+import factorio.model.Recipe;
+import factorio.model.ItemStack;
 
 public class JacksonTest {
+
+    private ObjectMapper mapper;
+
     record Person(
         String name,
         int age
     ){}
 
+    @BeforeEach
+    void setUp(){
+        mapper=new ObjectMapper();
+    }
+
     @Test
     void readPerson() throws Exception{
-        ObjectMapper mapper=new ObjectMapper();
         Person person=mapper.readValue(new File("src/test/resources/person.json"),Person.class);
 
         assertEquals("Alice",person.name());
@@ -24,7 +36,10 @@ public class JacksonTest {
 
     @Test
     void readRecipe() throws Exception{
-        ObjectMapper mapper=new ObjectMapper();
-        
+        Recipe recipe = mapper.readValue(new File("src/test/resources/recipe.json"),Recipe.class);
+
+        assertEquals(List.of(new ItemStack("iron_ore",1)),recipe.ingredients());
+        assertEquals(List.of(new ItemStack("iron_plate",1)),recipe.products());
+        assertEquals(3.2,recipe.craftingTime());
     }
 }
