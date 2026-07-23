b746ce7ef0ac5bf3e04652ee954156d86b0843a2
math-taka <mass.11235813@gmail.com>
2026-07-23 01:19:08 +0000

    setting readRecipeListTest

--- src/test/java/factorio/json/JacksonTest.java
+++ src/test/java/factorio/json/JacksonTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.util.List;
@@ -41,5 +42,26 @@ public class JacksonTest {
         assertEquals(List.of(new ItemStack("iron_ore",1)),recipe.ingredients());
         assertEquals(List.of(new ItemStack("iron_plate",1)),recipe.products());
         assertEquals(3.2,recipe.craftingTime());
+    }
+
+    @Test
+    void readRecipeList() throws Exception{
+        File file = new File("src/test/resources/recipelist.json");
+        List<Recipe> recipeList = mapper.readValue(file,new TypeReference<>() {});
+
+        Recipe recipe0=new Recipe(
+            List.of(new ItemStack("iron_ore",1)),
+            List.of(new ItemStack("iron_plate",1)),
+            3.2
+        );
+
+        Recipe recipe1=new Recipe(
+            List.of(new ItemStack("iron_plate",2)),
+            List.of(new ItemStack("gear_wheel",1)),
+            0.5
+        );
+        
+        assertEquals(recipe0,recipeList.get(0));
+        assertEquals(recipe1,recipeList.get(1));
     }
 }
