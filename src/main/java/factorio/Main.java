22b4ce3fe0a94745a5c90d85540471797a710a88
math-taka <mass.11235813@gmail.com>
2026-07-26 08:14:24 +0000

    ReadyForDemoOfRecipeCalculator

--- src/main/java/factorio/Main.java
+++ src/main/java/factorio/Main.java
@@ -1,45 +1,49 @@
 package factorio;
 
 import java.util.List;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.IOException;
 
 import factorio.model.ItemStack;
 import factorio.model.Recipe;
 import factorio.model.RecipeBook;
 import factorio.calculator.RecipeCalculator;
+import factorio.io.RecipeLoader;
 
 public class Main{
     public static void main(String[] args){
 
-        ItemStack needs = new ItemStack("Transport Belt", 10);
+        String[] recipeNames ={
+            "iron_plate", "copper_plate", "steel_plate", "copper_cable", "gear_wheel", "electronic_circuit",
+            "automation_science_pack", "transport_belt", "inserter", "logistic_science_pack"
+        };
 
-        ItemStack ironOre = new ItemStack("Iron Ore",1);
-        ItemStack ironPlate = new ItemStack("Iron Plate",1);
-
-        Recipe ironPlateRecipe = new Recipe(List.of(ironOre),List.of(ironPlate),3.2);
-
-        ItemStack ironPlateForGear = new ItemStack("Iron Plate",2);
-        ItemStack GearWheel = new ItemStack("Gear Wheel",1);
-
-        Recipe gearWheelRecipe = new Recipe(List.of(ironPlateForGear),List.of(GearWheel),0.5);
-
-        ItemStack gearForBelt = new ItemStack("Gear Wheel",1);
-        ItemStack ironPlateForBelt = new ItemStack("Iron Plate",1);
-        ItemStack TransportBelt = new ItemStack("Transport Belt",2);
+        String pathHead = "src/main/resources/recipes/", jsonIdentifier=".json";
         
-        Recipe transportBeltRecipe = new Recipe(
-            List.of(gearForBelt,ironPlateForBelt),
-            List.of(TransportBelt),0.5
+        ArrayList<Recipe> recipes = new ArrayList<>();
+
+        for(String recipeName:recipeNames){
+            File recipeFile = new File(pathHead+recipeName+jsonIdentifier);
+            try{
+                recipes.add(RecipeLoader.loadRecipe(recipeFile));
+            }catch(IOException e){
+                System.err.println(e);
+            }
+        }
+
+        RecipeBook book = new RecipeBook(recipes);
+
+        List<ItemStack> needs = List.of(
+            new ItemStack("automation_science_pack",1),
+            new ItemStack("logistic_science_pack",1)
         );
 
-        RecipeBook selected=new RecipeBook(
-            List.of(ironPlateRecipe,gearWheelRecipe,transportBeltRecipe)
-        );
-        
-        RecipeCalculator calculator = new RecipeCalculator();
+        List<ItemStack> ingredients = new RecipeCalculator().calculateIngredients(needs, book);
 
-        List<ItemStack> result=calculator.calculateIngredients(needs, selected);
-
-        System.out.println(result);
-        
+        for(ItemStack ingredient:ingredients){
+            System.out.println(ingredient);
+        }
     }
 }
